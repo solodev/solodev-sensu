036f4255a993451916f1c1f303934d96fbc3113a
Sean Porter <portertech@gmail.com>
2016-09-15 10:10:03 -0700

    customer client rabbitmq hosts overriden by custom json

--- recipes/customer_client.rb
+++ recipes/customer_client.rb
@@ -4,14 +4,6 @@
 #
 # Copyright (c) 2016 Solodev, All Rights Reserved.
 
-# The following RabbitMQ hosts must be manually updated, as customer
-# OpsWorks stacks are unable to discover the active Sensu RabbitMQ
-# cluster nodes.
-node.override["sensu"]["rabbitmq"]["hosts"] = []
-
-# TODO: Figure out secrets management for customer OpsWorks stacks,
-# for the Sensu client SSL certificate/key. Currently using citadel
-# with the Sensu OpsWorks stack S3 bucket.
 set_sensu_state(node, "ssl", "client", "cert", citadel["sensu/ssl/client/cert.pem"])
 set_sensu_state(node, "ssl", "client", "key", citadel["sensu/ssl/client/key.pem"])
 
