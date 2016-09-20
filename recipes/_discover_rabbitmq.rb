2bab14b770c1ad4281051bfe6229ddc7d9771ba1
Sean Porter <portertech@gmail.com>
2016-09-19 17:11:08 -0700

    updated rabbitmq discovery for multiple follower layers

--- recipes/_discover_rabbitmq.rb
+++ recipes/_discover_rabbitmq.rb
@@ -7,14 +7,18 @@
 leader_layer = Chef::DataBagItem.load("aws_opsworks_layer", "rabbitmq-leader").to_hash
 leader_layer_id = leader_layer["layer_id"]
 
-follower_layer = Chef::DataBagItem.load("aws_opsworks_layer", "rabbitmq-follower").to_hash
-follower_layer_id = follower_layer["layer_id"]
+follower_one_layer = Chef::DataBagItem.load("aws_opsworks_layer", "rabbitmq-follower-one").to_hash
+follower_one_layer_id = follower_one_layer["layer_id"]
+
+follower_two_layer = Chef::DataBagItem.load("aws_opsworks_layer", "rabbitmq-follower-two").to_hash
+follower_two_layer_id = follower_two_layer["layer_id"]
 
 opsworks_instances = search("aws_opsworks_instance")
 
 rabbitmq_nodes = opsworks_instances.select { |instance|
   instance["layer_ids"].include?(leader_layer_id) ||
-  instance["layer_ids"].include?(follower_layer_id)
+  instance["layer_ids"].include?(follower_one_layer_id) ||
+  instance["layer_ids"].include?(follower_two_layer_id)
 }.map { |instance| instance.to_hash }
 
 rabbitmq_leader_nodes = opsworks_instances.select { |instance|
