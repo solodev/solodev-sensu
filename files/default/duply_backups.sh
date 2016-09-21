351f3ddb8199c54fab5c0770fa33276dd1eaf8ce
Sean Porter <portertech@gmail.com>
2016-09-21 08:59:18 -0700

    pass duply backup arguments through to duply in wrapper

--- files/default/duply_backups.sh
+++ files/default/duply_backups.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUTPUT="$(duply backup backup)"
+OUTPUT="$(duply $@)"
 STATUS=$?
 
 echo "{\"name\": \"cron_duply_backups\", \"output\": \"$OUTPUT\", \"status\": $STATUS}" | nc localhost 3030
